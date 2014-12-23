9355587ef7f642f9041bff4ce2664030a26dbe36
Kyle Fuller <inbox@kylefuller.co.uk>
2014-12-23 20:21:34 +0000

    Release 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rivr-peewee',
-    version='0.1.0',
+    version='0.1.1',
     description='rivr integration for the peewee database ORM.',
     url='https://github.com/rivrproject/rivr-peewee',
     packages=find_packages(),
